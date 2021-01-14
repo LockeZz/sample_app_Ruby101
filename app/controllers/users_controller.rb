882bfaa16004f95682068ee0a4c855f1d6551a46
p2digital <p2digital@p2digitals-MacBook-Pro.local>
2021-01-14 18:01:51 +0800

    deleting-user completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   
   def new
     @user = User.new
@@ -39,6 +40,12 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  def destroy 
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to  users_path
+  end
+
   private
 
   def user_params
@@ -56,5 +63,9 @@ class UsersController < ApplicationController
       flash[:error] = "Please log in."
       redirect_to login_path
     end
+  end
+
+  def admin_user 
+    redirect_to(root_path) unless current_user.admin?
   end
 end
