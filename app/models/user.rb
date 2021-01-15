f05823a9912e072d57775b0811ec807c5cd904b9
p2digital <p2digital@p2digitals-MacBook-Pro.local>
2021-01-15 20:41:49 +0800

    micropost creation and home page worked

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,19 +61,25 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, nil)
     end
 
-    # Converts email to all lower-case.
-    def downcase_email
-        self.email = email.downcase
-    end
-
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-        self.activation_token  = User.new_token
-        self.activation_digest = User.digest(activation_token)
-    end
-
     #Returns true if a password reset has expired
     def password_reset_expired?
         reset_sent_at < 2.hours.ago
     end
+
+    def feed 
+        Micropost.where("user_id = ?", id)
+    end
+
+    private 
+
+        # Converts email to all lower-case.
+        def downcase_email
+            self.email = email.downcase
+        end
+
+        # Creates and assigns the activation token and digest.
+        def create_activation_digest
+            self.activation_token  = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
 end
