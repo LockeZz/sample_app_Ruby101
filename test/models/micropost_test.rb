523bd2b3ae583e357c968fda611f65c023ad9432
p2digital <p2digital@p2digitals-MacBook-Pro.local>
2021-01-15 19:32:28 +0800

    micropost modelling completed

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,9 +3,8 @@ require "test_helper"
 class MicropostTest < ActiveSupport::TestCase
   
   def setup
-    @user = users(:michael)
-    #This code is not idiomaticall correct. 
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @user = users(:hfpang)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -15,6 +14,20 @@ class MicropostTest < ActiveSupport::TestCase
   test "user id should be present" do
     @micropost.user_id = nil 
     assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" *141
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 
 end
