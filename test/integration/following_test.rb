7e06da09ca1777c5c4dffea4243b03cf6f7a9ef3
p2digital <p2digital@p2digitals-MacBook-Pro.local>
2021-01-18 12:00:40 +0800

    follow and unfollow button completed

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,13 +4,14 @@ class FollowingTest < ActionDispatch::IntegrationTest
   
   def setup 
     @user = users(:hfpang)
+    @other = users(:michael)
     log_in_as(@user)
   end
 
   test "following page" do
     get following_user_path(@user)
     assert_not @user.following.empty?
-    assert_match @user.following.count.to_S, response.body 
+    assert_match @user.following.count.to_s, response.body 
     @user.following.each do |user|
       assert_select 'a[href=?]', user_path(user)
     end
@@ -24,5 +25,20 @@ class FollowingTest < ActionDispatch::IntegrationTest
       assert_select "a[href=?]", user_path(user)
     end
   end
+  
+  test "should follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: {followed_id: @other.id}
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
 
 end
